7d33d1a966f4d65c536b014c31b5c17cf110caed
Fatma Tarek Abdallah Sherif <ftarek@ISFPDOMAIN.com>
2021-07-28 15:45:30 +0200

    different between template vs templateUrl and styleUrl vs styles

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -3,7 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css'],
+  /************************
+   * if you want style inside ts 
+   * use back ticks for multiple lines 
+   *************************/
+  styles: [`
+    h3 {
+      color: red
+    }
+  
+  `]
 })
 export class AppComponent {
   
--- my-app/src/app/servers/servers.component.ts
+++ my-app/src/app/servers/servers.component.ts
@@ -3,12 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
+  /**************************
+   * if you want add html inside ts so use template instead templateUrl
+   * to use js use back tic
+  **************************/
+  // template:`<app-server><app-server>`,
   styleUrls: ['./servers.component.css']
 })
+
 export class ServersComponent implements OnInit {
-
   constructor() { }
-
   ngOnInit(): void {
   }
 

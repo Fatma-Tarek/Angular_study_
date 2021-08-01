73ef539838a5d4a1bf4dd54258f677d17396836e
Fatma Tarek Abdallah Sherif <ftarek@ISFPDOMAIN.com>
2021-08-01 13:21:54 +0200

    event and property databinding @Input @Output, style encapuslation

---
+++ components and databinding/app-section5/src/app/cokpit/cokpit.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit ,EventEmitter, Output } from '@angular/core';
+
+
+@Component({
+  selector: 'app-cokpit',
+  templateUrl: './cokpit.component.html',
+  styleUrls: ['./cokpit.component.css'],
+  
+})
+export class CokpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{serverName: string , serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string , serverContent: string}>();
+  constructor() { }
+
+  newServerName = '';
+  newServerContent = '';
+
+  ngOnInit(): void {
+  }
+
+  onAddServer() {
+   this.serverCreated.emit({
+      serverName: this.newServerName ,
+      serverContent: this.newServerContent
+   });
+  }
+
+  onAddBlueprint() {
+    this.blueprintCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    })
+  }
+
+}

00607d3e8b07b1951295494e2191e41ff9ada349
Fatma Tarek Abdallah Sherif <ftarek@ISFPDOMAIN.com>
2021-08-04 14:29:46 +0200

    declare routes and add functionality to our components

---
+++ routers/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './users/user/user.component';
+import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './servers/server/server.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServersService } from 'src/app/servers/servers.service'
+import { FormsModule } from '@angular/forms';
+
+// will hold all my routes in my appplication
+const appRoutes: Routes= [
+  {path:'', component: HomeComponent },
+  {path:'users', component: UsersComponent },
+  {path:'servers', component: ServersComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    HomeComponent,
+    ServersComponent,
+    ServerComponent,
+    EditServerComponent,
+  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [ServersService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

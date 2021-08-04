04b1205daa7a0572e28bcaefe3a84926b2aeef08
Fatma Tarek Abdallah Sherif <ftarek@ISFPDOMAIN.com>
2021-08-05 00:17:50 +0200

    add Query parameters and fragments dynamically and using html

--- routers/src/app/app.module.ts
+++ routers/src/app/app.module.ts
@@ -18,7 +18,10 @@ import { FormsModule } from '@angular/forms';
 const appRoutes: Routes= [
   {path:'', component: HomeComponent },
   {path:'users', component: UsersComponent },
-  {path:'servers', component: ServersComponent}
+  {path: 'users/:id/:name', component: UserComponent},
+  {path:'servers', component: ServersComponent},
+  {path:'servers/:id/edit', component: EditServerComponent}
+
 ];
 
 @NgModule({

66bd39764b7220b1ecb0e84ca92bb81928b992a4
Fatma Tarek Abdallah Sherif <ftarek@ISFPDOMAIN.com>
2021-08-05 12:07:46 +0200

    outsourcing the route

--- routers/src/app/app.module.ts
+++ routers/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,22 +12,10 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServersService } from 'src/app/servers/servers.service'
 import { FormsModule } from '@angular/forms';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRountingModule } from './app-routing.nodule';
 
-// will hold all my routes in my appplication
-// not to add / before routes ex: /users
-const appRoutes: Routes= [
-  {path:'', component: HomeComponent },
-  {path:'users', component: UsersComponent, 
-        children:[{path: ':id/:name', component: UserComponent}] 
-  },
-  {path:'servers', component: ServersComponent,
-        children:[ {path: ':id', component: ServerComponent},
-        {path:':id/edit', component: EditServerComponent}]
-  },
-  //page not found so use redirect and match all path
-  {path:'not-found', component:PageNotFoundComponent},
-  {path:'**', redirectTo:'/not-found', pathMatch: 'full' }
-];
+
+
 
 @NgModule({
   declarations: [
@@ -46,7 +33,7 @@ const appRoutes: Routes= [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRountingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]

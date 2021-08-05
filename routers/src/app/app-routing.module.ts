a999bb5ef71a1aab86ef85e3fe140b095bb3c164
Fatma Tarek Abdallah Sherif <ftarek@ISFPDOMAIN.com>
2021-08-05 17:17:14 +0200

    add ststic data to rounting

--- routers/src/app/app-routing.module.ts
+++ routers/src/app/app-routing.module.ts
@@ -20,6 +20,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AuthGuard } from './auth-guard.service';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 
 
@@ -35,10 +36,12 @@ const appRoutes: Routes= [
                      {path:':id/edit', component: EditServerComponent,  canDeactivate: [CanDeactivateGuard]}]
     },
     //page not found so use redirect and match all path
-    {path:'not-found', component:PageNotFoundComponent},
+    //{path:'not-found', component:PageNotFoundComponent},
+    {path:'not-found', component:ErrorPageComponent, data: {message: 'Page Not Found'}},
     {path:'**', redirectTo:'/not-found', pathMatch: 'full' }
   ];
 
+  
 @NgModule({
     imports:[  
        RouterModule.forRoot(appRoutes)
